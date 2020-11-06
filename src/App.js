7340ea4102ccad79f016cc1a52b257ce3c488352
s-282 <yadav.sonal282@gmail.com>
2020-11-06 20:52:12 +0530

    Added login and Signup Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import "./App.css";
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+//import { BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+
+import Navbar from "./components/navbar.component"
+import Login from "./components/Auth/Login";
+import Signup from "./components/Auth/Signup";
+/*import Navbar from "./components/navbar.component"
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
+*/
 
 function App() {
   return (
-    <div>
-      <h1>Hello gaiz :)</h1>
-    </div>
+    <Router>
+      <div className="container">
+      <Navbar />
+      <br/>
+      <Route path="/login"  component={Login} />
+      <Route path="/signup" component={Signup} />
+      </div>
+    </Router>
   );
 }
 
