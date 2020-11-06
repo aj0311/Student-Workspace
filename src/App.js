0cea5dca6acb9eef7ad588fe04075349355eaae5
Tanish Agarwal <tanishagarwal81201@gmail.com>
2020-11-06 21:16:49 +0530

    made necessary changes

--- src/App.js
+++ src/App.js
@@ -1,36 +1,24 @@
-
-import React from 'react';
-import "bootstrap/dist/css/bootstrap.min.css";
-//import { BrowserRouter as Router, Route} from "react-router-dom";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-
-import Navbar from "./components/navbar.component"
-import Login from "./components/Auth/Login";
-import Signup from "./components/Auth/Signup";
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/navbar.component";
+import Footer from "./components/Footer.component";
+import Login from "./components/Auth/Login";
+import Signup from "./components/Auth/Signup";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
   return (
     <Router>
-
-      <div className="container">
-      <Navbar />
-      <br/>
-      <Route path="/login"  component={Login} />
-      <Route path="/signup" component={Signup} />
+      <div>
+        <Navbar />
+        <br />
+        <Route exact path="/" component={Login} />
+        <Route exact path="/signup" component={Signup} />
+        <br />
+        <Footer />
       </div>
-      <Navbar />
-      <br />
-      <div className="container">
-        <Route path="/" exact component={Home} />
-        <Route path="/workspace/:id" exact component={Dashboard} />
-      </div>
-      <br />
-      <Footer />
     </Router>
   );
 }
--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class Navbar extends Component {
-render() {
+  render() {
     return (
-        <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-            <Link to="/" className="btn btn-outline-primary my-2 my-sm-0">Home</Link>
-            <Link to="/login" className="btn btn-outline-primary my-2 my-sm-0">Login</Link>
-            <Link to="/signup" className="btn btn-outline-primary my-2 my-sm-0">Signup</Link>
+      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        <Link to="/" className="btn btn-outline-primary my-2 my-sm-0">
+          Login
+        </Link>
+        <Link to="/signup" className="btn btn-outline-primary my-2 my-sm-0">
+          Signup
+        </Link>
       </nav>
     );
   }
