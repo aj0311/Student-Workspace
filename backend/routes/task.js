53b13a686e6b0630167e00b5cd05515074fed387
Tanish Agarwal <tanishagarwal81201@gmail.com>
2020-11-07 14:47:38 +0530

    updated local changes to repo

--- backend/routes/task.js
+++ backend/routes/task.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const Task = require("../models/task");
 const auth = require("../middleware/auth");
-const config = require('config');
+const config = require("config");
 const router = express.Router();
-const User = require('../models/user');
+const User = require("../models/user");
 
 //! create task
-router.post('/', auth, async (req, res) => {
-  const user = await User.findById(req.user.id).select('-password');
+router.post("/", auth, async (req, res) => {
+  const user = await User.findById(req.user.id).select("-password");
   const task = new Task({
     ...req.body,
-    taskOwner: user._id
+    taskOwner: user._id,
   });
   try {
     await task.save();
@@ -25,13 +25,13 @@ router.post('/', auth, async (req, res) => {
 // GET /tasks?limit=10&skip=0   (pagination)
 // GET /tasks?sortBy=createdAt_asc
 // ?sortBy=createdAt:asc&completed=true&limit=1&skip=1
-router.get('/', auth, async (req, res) => {
-    const user = await User.findById(req.user.id).select('-password');
+router.get("/", auth, async (req, res) => {
+  const user = await User.findById(req.user.id).select("-password");
   try {
-    const tasks = await Task.find({owner: req.user._id}).sort({ date: -1 });
+    const tasks = await Task.find({ owner: req.user._id }).sort({ date: -1 });
     if (!task) {
-        return res.status(404).send("No task found or you're not authenticated");
-      }
+      return res.status(404).send("No task found or you're not authenticated");
+    }
     res.send(tasks);
   } catch (error) {
     res.status(500).send();
@@ -44,7 +44,9 @@ router.get("/:id", auth, async (req, res) => {
   try {
     const task = await Task.findOne({ _id, owner: req.user._id });
     if (!task) {
-      return res.status(404).send("Task not found or you're not authenticated");
+      return res
+        .status(404)
+        .send("Task not founded or you're not authenticated");
     }
     res.send(task);
   } catch (error) {
@@ -56,8 +58,10 @@ router.get("/:id", auth, async (req, res) => {
 router.patch("/:id", auth, async (req, res) => {
   //*
   const updates = Object.keys(req.body);
-  const allowedUpdates = ["taskName","taskDescription","taskStatus"];
-  const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+  const allowedUpdates = ["taskName", "taskDescription", "taskStatus"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
   if (!isValidOperation) {
     return res.status(400).send("ERROR: invalid operation");
   }
@@ -85,7 +89,7 @@ router.delete("/:id", auth, async (req, res) => {
   try {
     const task = await Task.findOneAndDelete({ _id, owner: req.user._id });
     if (!task) {
-      res.status(404).send("Task not founded or You're not authenticated");
+      res.status(404).send("Task not found or You're not authenticated");
     }
     res.send(task);
   } catch (error) {
