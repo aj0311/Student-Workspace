dbc2850ece93b762029c2566bcda41bb73ab611e
s-282 <yadav.sonal282@gmail.com>
2020-11-07 14:23:58 +0530

    Modified Login/Signup page

--- src/components/Auth/Signup.js
+++ src/components/Auth/Signup.js
@@ -58,9 +58,15 @@ export default class Signup extends Component {
   render() {
     return (
       <div id="page" class="row mb-2">
-        <div  class="col-md-6">
-          <div class="jumbotron">
-            <h1>Navbar example</h1>
+        <div class="col-md-6">
+          <div class="jumbotron ">
+            <div id="img1">
+              <svg width="6em" height="6em" viewBox="0 0 16 16" class="bi bi-book-half" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M8.5 2.687v9.746c.935-.53 2.12-.603 3.213-.493 1.18.12 2.37.461 3.287.811V2.828c-.885-.37-2.154-.769-3.388-.893-1.33-.134-2.458.063-3.112.752zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z" />
+              </svg>
+            </div>
+
+            <h1>WORKSPACE</h1>
             <p class="lead">This example is a quick exercise to illustrate how fixed to top navbar works. As you scroll, it will remain fixed to the top of your browser’s viewport.</p>
           </div>
         </div>
@@ -96,7 +102,7 @@ export default class Signup extends Component {
               />
             </div>
             <div className="form-group">
-              <input type="submit" value="Signup" className="btn btn-primary" />
+              <input type="submit" value="Signup" id="bton" className="btn btn-primary" />
             </div>
           </form>
         </div>
